8d1b5d61ef45d83767f78c394e8360db5e3ec226
Tonii18 <ajsanrosal@gmail.com>
2025-10-23 14:10:12 +0200

    Trying something

--- src/log_analyzer/analyzer.py
+++
@@ -1,8 +0,0 @@
-class Analyzer:
-
-    def __init__(self):
-        pass
-
-    def run(self):
-        file = input('Enter the file you want to analyse: ')
-        print('Getting ready to analyse '+file)
